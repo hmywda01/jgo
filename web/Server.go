a11481c0da391c9e5f7a4f941d8092e2d3041ba5
hmywda01 <hmywda01@11.com>
2021-10-11 20:47:04 +0800

    modify listen port to tcp4

--- web/Server.go
+++ web/Server.go
@@ -203,8 +203,7 @@ func getResponse(w http.ResponseWriter, r *http.Request, s *Server, static bool)
 func (s *Server) startServer() error {
 	srv := &http.Server{
 		Handler: s.router,
-		Addr:    ":" + strconv.Itoa(s.Port),
-		AddrType: net.TCP4,
+		Addr:    "0.0.0.0:" + strconv.Itoa(s.Port),
 	}
 	jlog.Logf("Starting server on port %d...\n", s.Port)
 	return srv.ListenAndServe()
